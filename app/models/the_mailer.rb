c850b1776f12f7e4f9fde9d160db34b1d7852bf9
altay <aguvench@gmail.com>
2010-06-28 03:05:47 -0700

    changed email + recipients

--- app/models/the_mailer.rb
+++ app/models/the_mailer.rb
@@ -1,7 +1,7 @@
 class TheMailer < ActionMailer::Base
   
   def new_name_notification(name)
-    recipients "aguvench@gmail.com"
+    recipients ["aguvench@gmail.com", "officerfishdumplings@me.com", "scottwroy@gmail.com", "sf@shawnfeeney.com"]
     from       "aguvench@gmail.com"
     subject    "#{name.user.login} suggested a new name!"
     sent_on    Time.now
